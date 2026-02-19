4524929ac4f6a8b2ad39bfb280c16da1ac166053
LockHurb <zdiaz@espol.edu.ec>
2026-02-18 22:29:50 -0500

    feat: Modo oscuro en texto

--- countries-dashboard/src/components/CountryCard.jsx
+++ countries-dashboard/src/components/CountryCard.jsx
@@ -2,20 +2,36 @@ import React from 'react';
 
 const CountryCard = ({ country, onClick }) => {
     return (
-        <article onClick={() => onClick(country)} className="bg-white rounded-lg shadow-md cursor-pointer hover:shadow-xl hover:-translate-y-1 transition-all duration-300 overflow-hidden">
-        {/* Imagen de bandera */}
-        <img src={country.flags.png} alt={`Bandera de ${country.name.common}`} className="w-full h-40 object-cover"
-        />
+        <article 
+            onClick={() => onClick(country)}
+            className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden cursor-pointer hover:shadow-xl hover:-translate-y-1 transition-all duration-300"
+        >
+            <img 
+                src={country.flags.png} 
+                alt={`Bandera de ${country.name.common}`} 
+                className="w-full h-40 object-cover"
+            />
 
-        <h3 className="font-extrabold text-lg pt-6 px-6 pb-2">{country.name.common}</h3>
-        
-        <div className="px-6 pb-6 text-sm text-gray-700">
-            <p className="mb-1"><strong className="font-semibold text-gray-900">Población:</strong> {country.population.toLocaleString()}</p>
-            <p className="mb-1"><strong className="font-semibold text-gray-900">Región:</strong> {country.region}</p>
-            <p className="mb-1"><strong className="font-semibold text-gray-900">Capital:</strong> {country.capital ? country.capital[0] : 'N/A'}</p>
-        </div>
+            <div className="p-6 pb-8 text-gray-900 dark:text-white">
+                <h3 className="font-extrabold text-lg mb-4">{country.name.common}</h3>
+                
+                <div className="text-sm space-y-1 text-gray-700 dark:text-gray-300">
+                    <p>
+                        <span className="font-semibold text-gray-900 dark:text-white">Population: </span>
+                        {country.population.toLocaleString()}
+                    </p>
+                    <p>
+                        <span className="font-semibold text-gray-900 dark:text-white">Region: </span>
+                        {country.region}
+                    </p>
+                    <p>
+                        <span className="font-semibold text-gray-900 dark:text-white">Capital: </span>
+                        {country.capital ? country.capital[0] : 'N/A'}
+                    </p>
+                </div>
+            </div>
         </article>
-  );
+    );
 };
 
 export default CountryCard;
